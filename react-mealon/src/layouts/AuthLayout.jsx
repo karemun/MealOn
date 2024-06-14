c420f1eef6a01757cb9ab4cc8754be3d297dc0f6
Karen Flores <karemu.8910@gmail.com>
2024-06-14 14:42:06 -0600

    Crear vistas para Login y Registro

--- react-mealon/src/layouts/AuthLayout.jsx
+++ react-mealon/src/layouts/AuthLayout.jsx
@@ -2,11 +2,12 @@ import { Outlet } from 'react-router-dom'
 
 export default function AuthLayout() {
   return (
-    <div>
-      AuthLayout
+    <main className='max-w-4xl m-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center'>
+      <img src="../img/logo-cat.svg" alt="Logotipo" className='max-w-xs' />
 
-      <Outlet />
-      
-    </div>
+      <div className='p-10 w-full'>
+        <Outlet />
+      </div>
+    </main>
   )
 }
