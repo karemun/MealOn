64c5c58962c7eb393991f41267eb7f2ff9f9c8e2
Karen Flores <karemu.8910@gmail.com>
2024-06-16 21:40:50 -0600

    Crear un Context para manejar el estado de la app

--- react-mealon/src/components/Categoria.jsx
+++ react-mealon/src/components/Categoria.jsx
@@ -1,18 +1,27 @@
-
+import useQuiosco from "../hooks/useQuiosco"
 
 export default function Categoria({ categoria }) {
 
+    const { handleClickCategoria, categoriaActual } = useQuiosco()
     const { nombre, icono, id } = categoria
 
+    const resaltarCategoriaActual = () => categoriaActual?.id === id ? 'bg-amber-400' : 'bg-white'
+
     return (
-        <div className="flex items-center gap-4 border w-full p-3 hover:bg-amber-400 cursor-pointer">
+        <div className={`${ resaltarCategoriaActual() } flex items-center gap-4 border w-full p-3 hover:bg-amber-400 cursor-pointer`}>
             <img 
                 src={`/img/icono_${icono}.svg`}
                 className="w-12"
                 alt="Icono"
             />
 
-            <p className="text-lg font-bold cursor-pointer truncate">{ nombre }</p>
+            <button 
+                className="text-lg font-bold cursor-pointer truncate"
+                type="button"
+                onClick={() => handleClickCategoria(id)}
+            >
+                { nombre }
+            </button>
         </div>
     )
 }
