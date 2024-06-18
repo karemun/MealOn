014e983cbc7d3154bb4d29eb380415caa46aa206
Karen Flores <karemu.8910@gmail.com>
2024-06-18 15:17:56 -0600

    Habilitar CORS en Laravel para que acepte peticiones de React

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\RegistroRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\RegistroRequest;
 
 class AuthController extends Controller
 {
     public function register(RegistroRequest $request)
     {
         $data = $request->validated(); // Llama a rules de la clase RegistroRequest
+
+        // Crear usuario
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        // Retornar respuesta
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
     }
 
     public function login(Request $request)
